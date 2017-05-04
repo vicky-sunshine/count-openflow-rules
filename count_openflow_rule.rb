980457ed77f8ac275c7477ce01d833f43f9e2dff
vicky <lch82327@gmail.com>
2017-05-04 12:23:39 +0800

    add iperf rule

--- count_openflow_rule.rb
+++ count_openflow_rule.rb
@@ -11,12 +11,16 @@ ssh = Net::SSH.start('192.168.2.200', 'admin', :password=> '123456')
 open('rule.tsv', 'w') do |f|
 
   count = 0
-  boundary = 70
+  boundary = 100
+  puts "time\t    rules\tiperf_rules"
+  f << "time\t    rules\tiperf_rules"
   while count < boundary
     time = ssh.exec!('date').split()
-    rules = ssh.exec!("/ovs/bin/ovs-ofctl dump-flows br0 | grep table | wc -l")
-    puts "#{time[3]}\t#{rules}"
-    f << "#{time[3]}\t#{rules}"
+    rules = ssh.exec!("/ovs/bin/ovs-ofctl dump-flows br0 | grep table | wc -l").delete("\n")
+    rules140 = ssh.exec!("/ovs/bin/ovs-ofctl dump-flows br0 | grep 140.114.71.175 | wc -l")
+
+    puts "#{time[3]}\t#{rules}\t#{rules140}"
+    f << "#{time[3]}\t#{rules}\t#{rules140}"
     sleep 0.9
     count = count + 1
   end
