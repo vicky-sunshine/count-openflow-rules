e4068f003a68702187559775fbef2bd2a729ec98
vicky <lch82327@gmail.com>
2017-06-21 17:07:16 +0800

    record old data

--- count_openflow_rule.rb
+++ count_openflow_rule.rb
@@ -1,27 +1,23 @@
 # `gem install net-ssh` first
 require 'net/ssh'
 
-ssh = Net::SSH.start('192.168.2.200', 'admin', :password=> '123456')
+ssh = Net::SSH.start('192.168.2.201', 'admin', :password=> '123456')
 
 # # sync time with ntp server
 # ssh.exec!("ntpdate time.stdtime.gov.tw")
 # # write to bios time
 # ssh.exec!("hwclock -w")
+open("2017_0523/#{ARGV[0]}.tsv", 'w') do |f|
 
-open('rule.tsv', 'w') do |f|
-
-  count = 0
-  boundary = 100
   puts "time\t    rules\tiperf_rules"
   f << "time\t    rules\tiperf_rules"
-  while count < boundary
-    time = ssh.exec!('date').split()
+  while true
+    time = `date +%H:%M:%S`.delete("\n")
     rules = ssh.exec!("/ovs/bin/ovs-ofctl dump-flows br0 | grep table | wc -l").delete("\n")
-    rules140 = ssh.exec!("/ovs/bin/ovs-ofctl dump-flows br0 | grep 140.114.71.175 | wc -l")
+    rules140 = ssh.exec!("/ovs/bin/ovs-ofctl dump-flows br0 | grep 140.114.71.176 | wc -l")
 
-    puts "#{time[3]}\t#{rules}\t#{rules140}"
-    f << "#{time[3]}\t#{rules}\t#{rules140}"
-    sleep 0.9
-    count = count + 1
+    puts "#{time}\t#{rules}\t#{rules140}"
+    f << "#{time}\t#{rules}\t#{rules140}"
+    sleep 1
   end
 end
